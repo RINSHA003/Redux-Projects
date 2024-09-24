9f9d4fed7860f04eb88eef843fedbef0a072f3df
RINSHA003 <rinshaparappara123@gmail.com>
2024-09-24 14:39:34 +0530

    thunk

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useRef, useState } from 'react'
 import './App.css'
 import './index.css'
 
 // import Header from './Components/Header/Header'
 // import ProductList from './Components/ProductList/ProductList'
 import {ProductList,Header} from './Components'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchUser } from './Redux/CartSlice'
 // import CompB from './Components/CompB'
 
 
@@ -12,7 +14,21 @@ import {ProductList,Header} from './Components'
  
 
 function App() {
+
+
+  const dispatch=useDispatch()
+  const counterRef=useRef(1)
+  const {userDetail}=useSelector((state)=>state.cart)
+  useEffect(()=>{
+        dispatch(fetchUser(counterRef.current))
+  },[])
+
+  const adduser=()=>{
+    dispatch(fetchUser(++counterRef.current))
+  }
   // const welcome='welcome to the context api'
+
+  console.log(userDetail)
   return (
     <>
      <Header />
@@ -20,6 +36,9 @@ function App() {
       {/* <data.Provider value={welcome}>
           <CompB/>
       </data.Provider> */}
+      <button onClick={adduser}>add user</button>
+      
+      <pre style={{color:'black'}}>{JSON.stringify(userDetail,undefined,4)}</pre>
     </>
   )
 }
--- src/Redux/CartSlice.jsx
+++ src/Redux/CartSlice.jsx
@@ -1,60 +1,69 @@
-import {createSlice,createAsyncThunk} from '@reduxjs/toolkit'
-import axios from 'axios'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const fetchUser=createAsyncThunk("cart/fetchUser",async()=>{
-    const responce= await axios.get("https://jsonplaceholder.typicode.com/users")
-    return responce
-})
+export const fetchUser = createAsyncThunk("cart/fetchUser", async (id) => {
+    const response = await axios.get(`https://jsonplaceholder.org/posts/${id}`);
+    return response.data;
+});
 
+const INITIAL_STATE = {
+    cartList: [],
+    cartCount: 0,
+    userDetail: []
+};
 
-const INITIAL_STATE={
-    cartList:[],
-    cartCount:0
-}
-const cartSlice=createSlice({
-    name:'cart',
-    initialState:INITIAL_STATE,
-    reducers:{
-        addToCart :(state,action)=>{
-        //    state.cartCount=1
-        //    console.log(action)
-            const itemExist= state.cartList.find((item)=>item.id == action.payload.id)
-            if(itemExist){
-                state.cartList.forEach((item)=>{
-                    if(item.id == action.payload.id){
-                        item.count =1
-                    }
-                })
-            }
-            else{
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: INITIAL_STATE,
+    reducers: {
+        addToCart: (state, action) => {
+            const itemExist = state.cartList.find((item) => item.id == action.payload.id);
+            if (itemExist) {
+                itemExist.count++;
+            } else {
                 state.cartList.push({
                     ...action.payload,
-                    count:1
-                  })
+                    count: 1
+                });
             }
-             
+            state.cartCount = state.cartList.reduce((total, item) => total + item.count, 0);
         },
-        incrementCount:(state,action)=>{
-        //    state.cartCount += 1
-              const productID=action.payload
-              state.cartList.forEach((item)=>{
-                if(item?.id == productID){
-                    item.count ++
+        incrementCount: (state, action) => {
+            const productID = action.payload;
+            state.cartList.forEach((item) => {
+                if (item?.id == productID) {
+                    item.count++;
                 }
-              })
+            });
+            state.cartCount = state.cartList.reduce((total, item) => total + item.count, 0);
         },
-        decrementCount:(state,action)=>{
-        //    state.cartCount -=1
-              const productID=action.payload
-              state.cartList.forEach((item)=>{
-                if(item?.id == productID){
-                    item.count--
+        decrementCount: (state, action) => {
+            const productID = action.payload;
+            state.cartList.forEach((item) => {
+                if (item?.id == productID) {
+                    item.count--;
                 }
-              })
+            });
+            state.cartCount = state.cartList.reduce((total, item) => total + item.count, 0);
         }
-    }
-})
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchUser.pending, (state) => {
+                console.log('loading true');
+            })
+            .addCase(fetchUser.fulfilled, (state, action) => {
+                console.log('success');
+                console.log(action)
+                state.userDetail.push(action.payload)
+                
+            })
+            .addCase(fetchUser.rejected, (state) => {
+                console.log('end');
+            });
+    },
+});
 
-export const {incrementCount,decrementCount,addToCart} = cartSlice.actions
+export const { incrementCount, decrementCount, addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
